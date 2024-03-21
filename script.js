3ec6ae9b405fa48d9ef74aa9b13ec66864970693
Shiyeon Ku <c.monk@wustl.edu>
2024-03-21 09:30:35 -0500

    display of images
    images can now be listed in the div

--- script.js
+++ script.js
@@ -3,22 +3,23 @@
 //@TODO iterate through an array depending on the button
 //@TODO set target image and clicked image
 const images=[
-    "/Users/shiyeonku/Desktop/cse204SP2024/07-image-gallery-ShiyeonKu/images/catia-dombaxe-fKyYtenmvcw-unsplash.jpg",
-    "/Users/shiyeonku/Desktop/cse204SP2024/07-image-gallery-ShiyeonKu/images/danny-greenberg-VY6nWjvYq9g-unsplash.jpg",
-    "/Users/shiyeonku/Desktop/cse204SP2024/07-image-gallery-ShiyeonKu/images/gavin-wilson-U3ZtDbTPWpE-unsplash.jpg",
-    "/Users/shiyeonku/Desktop/cse204SP2024/07-image-gallery-ShiyeonKu/images/guogete-ENBbE0uGYgs-unsplash.jpg",
-    "/Users/shiyeonku/Desktop/cse204SP2024/07-image-gallery-ShiyeonKu/images/jacalyn-beales-TG7A8msMsKc-unsplash.jpg",
-    "/Users/shiyeonku/Desktop/cse204SP2024/07-image-gallery-ShiyeonKu/images/jack-prew-5bze5kVrfAU-unsplash.jpg",
-    "/Users/shiyeonku/Desktop/cse204SP2024/07-image-gallery-ShiyeonKu/images/jack-prew-feexj8sv1DM-unsplash.jpg",
-    "/Users/shiyeonku/Desktop/cse204SP2024/07-image-gallery-ShiyeonKu/images/jonathan-caliguire-55UHPGrgJGE-unsplash.jpg",
-    "/Users/shiyeonku/Desktop/cse204SP2024/07-image-gallery-ShiyeonKu/images/neom-m5Wj2ThjA14-unsplash.jpg",
-    "/Users/shiyeonku/Desktop/cse204SP2024/07-image-gallery-ShiyeonKu/images/spenser-sembrat-fBnuxrXzDs0-unsplash.jpg",
-    "/Users/shiyeonku/Desktop/cse204SP2024/07-image-gallery-ShiyeonKu/images/tamas-tokos-ou8-_71Qigc-unsplash.jpg",
-    "/Users/shiyeonku/Desktop/cse204SP2024/07-image-gallery-ShiyeonKu/images/tetiana-kobzeva-ohy4BjZo118-unsplash.jpg",
+    "images/catia-dombaxe-fKyYtenmvcw-unsplash.jpg",
+    "images/danny-greenberg-VY6nWjvYq9g-unsplash.jpg",
+    "images/gavin-wilson-U3ZtDbTPWpE-unsplash.jpg",
+    "images/guogete-ENBbE0uGYgs-unsplash.jpg",
+    "images/jacalyn-beales-TG7A8msMsKc-unsplash.jpg",
+    "images/jack-prew-5bze5kVrfAU-unsplash.jpg",
+    "images/jack-prew-feexj8sv1DM-unsplash.jpg",
+    "images/jonathan-caliguire-55UHPGrgJGE-unsplash.jpg",
+    "images/neom-m5Wj2ThjA14-unsplash.jpg",
+    "images/spenser-sembrat-fBnuxrXzDs0-unsplash.jpg",
+    "images/tamas-tokos-ou8-_71Qigc-unsplash.jpg",
+    "images/tetiana-kobzeva-ohy4BjZo118-unsplash.jpg",
 ]
-document.getElementById("close").addEventListener("click", closeImage());
-document.getElementById("next").addEventListener("click", nextImage());
-document.getElementById("prev").addEventListener("click", prevImage());
+document.getElementById("close").addEventListener("click", closeImage);
+document.getElementById("next").addEventListener("click", nextImage);
+document.getElementById("prev").addEventListener("click", prevImage);
+initialize();
 
 function closeImage(){
     //@set hidden class on the container
@@ -36,6 +37,7 @@ function prevImage(){
 function initialize(){
     //iterate through images
     let articles = document.createElement('section');
+    articles.setAttribute('id', 'imglist');
 
     for (i=0; i < images.length; i++){
         let img = document.createElement('img');
@@ -43,9 +45,9 @@ function initialize(){
 
         img.setAttribute('src', images[i]);
         img.setAttribute('alt', 'gallery image');
-
+        img.style.display = "block";
+        //console.log(img);
         articles.appendChild(img);
     }
-    articles.style.display = "block";
-    document.getElementById("img_gallery").appendChild(aritcles);
+    document.getElementById("img_gallery").appendChild(articles);
 }
